f2bae9da267a7ff0d996ba7fc85b2353db999cc2
davinnovation <davinnovation@gmail.com>
2016-11-17 17:05:20 +0900

    update playNotes
    solve delay problem with multi threading

--- nn_midi_trainer/main.py
+++ nn_midi_trainer/main.py
@@ -1,25 +1,24 @@
-import pyaudio
-import wave
 import time
-import numpy
 import os
+import playNotes
 from mido import MidiFile
 
-from playNotes import Pyplay
 
-mid = MidiFile('\sampleMidi\Merry Go Round of Life (piano solo).mid')
+mid = MidiFile(os.getcwd()+'\sampleMidi\Merry Go Round of Life (piano solo).mid')
 note = []
 timing = 0
 for i in range(0, 88):
     note.append(0)
 for i, track in enumerate(mid.tracks):
     print('Track {}: {}'.format(i, track.name))
+
 for message in track:
     event = message.bytes()  # event consists of [function, note, deltatime]
     if 144 <= event[0] < 160:  # 0x90~0x9F note_on
         note[event[1] - 20] = 1  # filenumber of Note C4 is 40 and number of Evete[1] C4 is 60. These diffrence is 20
         print event[1]
-        Pyplay.playmusic([event[1] - 20])
+        playNotes.playNotes(codes=[event[1] - 20]).play()
+        time.sleep(0.5)
     elif 128 <= event[0] < 144:  # 0x80~0x8F note_off
         note[event[1] - 20] = 0
         # print note
---
+++ nn_midi_trainer/playNotes/__init__.py
@@ -0,0 +1,63 @@
+import pyaudio
+import os
+import time
+import numpy
+
+global get_PyAudio
+get_PyAudio = pyaudio.PyAudio()
+
+class playNotes():
+    def __init__(self,codes=[40], chunk_size=2**15):
+        import wave
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        self._wfs = []
+
+        for code in codes:
+            self._wfs.append(wave.open(cwd + "\piano88\Piano 0" + str(code) + ".wav", 'rb'))
+
+
+    def play(self,onset=None):
+        """
+        Play is handled in the background (multithreading)
+        """
+        import threading
+        self.onset = onset
+        trial = 0
+        while trial < 3:
+            try:
+                trial += 1
+                self._thread_play = threading.Thread(target=self._play)
+                self._thread_play.start()
+                return
+            except Exception:
+                raise RuntimeError('Could not set up thread')
+
+    def _play(self):
+
+        def callback(in_data, frame_count, time_info, status):
+            datas = []
+            decodeddatas = []
+            for wf in self._wfs:
+                datas.append(wf.readframes(frame_count))
+            for data in datas:
+                decodeddatas.append(numpy.fromstring(data,numpy.int16))
+            data = (sum(decodeddatas) * 0.5).astype(numpy.int16)
+            return (data,pyaudio.paContinue)
+        self._stream = get_PyAudio.open(format=get_PyAudio.get_format_from_width(self._wfs[0].getsampwidth()),
+										channels=self._wfs[0].getnchannels(),
+										rate=self._wfs[0].getframerate(),
+										output=True,
+										stream_callback=callback)
+        while self._stream.is_active():
+			time.sleep(0.1)
+        self._stream.stop_stream()
+        self._stream.close()
+
+
+# example
+if __name__ == '__main__':
+    playNotes.playNotes(codes=[57]).play()
+    time.sleep(0.5)
+    playNotes.playNotes(codes=[57, 59]).play()
+    time.sleep(0.5)
+    playNotes.playNotes(codes=[57, 60, 63]).play()
