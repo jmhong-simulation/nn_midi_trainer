70b92457aed7b092a7bcd1ebcacb5c9a902584bb
jh0116j <jh0116j@gmail.com>
2016-11-19 21:49:45 +0900

    Update main.py
    일단 1박자를 4번을 나누어 재생할 수 있도록 만들었습니다.

--- nn_midi_trainer/main.py
+++ nn_midi_trainer/main.py
@@ -4,24 +4,53 @@ from mido import MidiFile
 
 from pyPlayMusic import pyPlayMusic
 
-
-mid = MidiFile(os.getcwd()+'\sampleMidi\Merry Go Round of Life (piano solo).mid')
-note = []
-timing = 0
-for i in range(0, 88):
-    note.append(0)
+#http://www.piano-midi.de/beeth.htm
+mid = MidiFile(os.getcwd()+'\sampleMidi\moonlight.mid')
+note=[]
+tmp_event=[]
+event_list=[]
+duration =0
+timing =0  #add all of ticks
+for i in range(0,88):  #initialize note
+   note.append(0)
+print ("Ticks per beat:"),
+print (mid.ticks_per_beat)
 for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
-
+    print('Track {} : {} '.format(i, track.name))
 for message in track:
-    event = message.bytes()  # event consists of [function, note, deltatime]
-    if 144 <= event[0] < 160:  # 0x90~0x9F note_on
-        note[event[1] - 20] = 1  # filenumber of Note C4 is 40 and number of Evete[1] C4 is 60. These diffrence is 20
-        print event[1]
-        pyPlayMusic.playNotes(codes=[event[1] - 20]).play()
-        time.sleep(0.3)
-    elif 128 <= event[0] < 144:  # 0x80~0x8F note_off
-        note[event[1] - 20] = 0
-        # print note
-    else:  # All function except Note_on, Note_off
-        continue
+    event= message.bytes()   #event consists of [function, note, velocity]
+    timing += message.time
+    if message.type == 'note_on' and note[event[1]-20] == 0: #Sometimes show only 'note_on' 
+        note[event[1]-20] =timing #  filenumber of Note C4 is 40 and number of Evete[1] C4 is 60. These diffrence is 20
+    elif  message.type == 'note_off' :
+        duration=(timing - note[event[1]-20])
+        event_list.append([ note[event[1]-20] , event[1]-20 , duration])
+        note[event[1]-20] = 0
+    elif message.type == 'note_on' and note[event[1]-20] != 0 :
+        duration=(timing - note[event[1]-20])
+        event_list.append([ note[event[1]-20] , event[1]-20 , duration])
+        note[event[1]-20] = 0
+    else:     #All functions is included except Note_on, Note_off
+           continue
+tick_start =0
+tick_end = timing
+event_list.sort()
+time_table=[]
+beat_list=[]
+beat=1             
+div_per_beat =4
+for i in range(len(event_list)):
+    if event_list[i][0] < beat * mid.ticks_per_beat/div_per_beat:
+        beat_list.append(event_list[i])
+    else :
+        beat_list.append(event_list[i])
+        time_table.append(beat_list[:])
+        for num in range(len(beat_list)):
+            beat_list.pop()
+        beat+=1
+#for i in range(len(time_table)) :
+#    print (i),(" : "),(time_table[i])
+for i in range(len(time_table)) :
+   for j in range(len(time_table[i])):
+      pyPlayMusic.playNotes(codes=[time_table[i][j][1]]).play()
+   time.sleep((80/(float(480))))
